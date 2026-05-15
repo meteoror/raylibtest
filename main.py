797cddefe0e5cb7e3c5d76c82acb26c17e1d95c3
meteoror <crashdown13542@gmail.com>
2026-05-15 11:39:06 -0400

    ok NOW i made everything

--- main.py
+++ main.py
@@ -43,33 +43,24 @@ def main():
 
     walls_coefficient_of_restitution = 0.95
     floor_coefficient_of_restitution = 0.8
-    mass = 1 # kg
     coefficient_of_friction = 0.8
     gravity = 400 # pixels per second per second
     class Ball:
-        def __init__(self, x, y, v_x, v_y, a_x, a_y, radius, color):
+        def __init__(self, x, y, v_x, v_y, a_x, a_y, mass, radius, color):
             self.x = x
             self.y = y
             self.v_x = v_x
             self.v_y = v_y
             self.a_x = a_x
             self.a_y = a_y
+            self.f_x = 0
+            self.f_y = 0
+            self.sliding = False
+            self.mass = mass
             self.radius = radius
             self.color = color
 
-    BALL_RADIUS = 25
-    ball_color = pr.Color(255, 255, 255, 255)
-
-    ball = Ball(400, 300, 0, 0, 0, gravity, BALL_RADIUS, ball_color)
-
-    '''
-    p_x = ball.x
-    p_y = ball.y
-    v_x = ball.v_x
-    v_y = ball.v_y
-    a_x = ball.a_x
-    a_y = ball.a_y
-    '''
+    ball = Ball(400, 300, 0, 0, 0, gravity, 1, 25, pr.Color(255, 255, 255, 255)) # ADD MASS
     
     pr.init_window(window_width, window_height, "raylib-py simple test")
     pr.set_target_fps(60)
@@ -77,34 +68,34 @@ def main():
     while not pr.window_should_close():
         dt = pr.get_frame_time()
 
-        on_floor = ball.y >= window_height - BALL_RADIUS
+        ball.sliding = ball.y >= window_height - ball.radius
 
-        f_x = 0
-        f_y = mass * gravity
+        ball.f_x = 0
+        ball.f_y = ball.mass * gravity
 
         if pr.is_key_down(pr.KEY_A) or pr.is_key_down(pr.KEY_LEFT):
-            f_x -= 500
+            ball.f_x -= 500
         if pr.is_key_down(pr.KEY_D) or pr.is_key_down(pr.KEY_RIGHT):
-            f_x += 500
+            ball.f_x += 500
         if pr.is_key_down(pr.KEY_W) or pr.is_key_down(pr.KEY_UP):
-            f_y -= 500
+            ball.f_y -= 500
         if pr.is_key_down(pr.KEY_S) or pr.is_key_down(pr.KEY_DOWN):
-            f_y += 500
+            ball.f_y += 500
 
-        ball.a_x = f_x / mass
-        ball.a_y = f_y / mass
+        ball.a_x = ball.f_x / ball.mass
+        ball.a_y = ball.f_y / ball.mass
 
         ball.x, ball.y, ball.v_x, ball.v_y = updatePosition(ball.x, ball.y, ball.v_x, ball.v_y, ball.a_x, ball.a_y, dt)
-        ball.x, ball.y, ball.v_x, ball.v_y = checkCollision(ball.x, ball.y, ball.v_x, ball.v_y, BALL_RADIUS, window_width, window_height, floor_coefficient_of_restitution, walls_coefficient_of_restitution)
-        ball.v_x = applyFriction(ball.v_x, coefficient_of_friction, mass, gravity, dt) if on_floor else ball.v_x
+        ball.x, ball.y, ball.v_x, ball.v_y = checkCollision(ball.x, ball.y, ball.v_x, ball.v_y, ball.radius, window_width, window_height, floor_coefficient_of_restitution, walls_coefficient_of_restitution)
+        ball.v_x = applyFriction(ball.v_x, coefficient_of_friction, ball.mass, gravity, dt) if ball.sliding else ball.v_x
 
         pr.begin_drawing()
         pr.clear_background(pr.Color(135, 206, 235, 255))
 
-        pr.draw_circle(int(ball.x), int(ball.y), BALL_RADIUS, ball_color)
+        pr.draw_circle(int(ball.x), int(ball.y), ball.radius, ball.color)
 
-        pr.draw_text(str(f_x), 10, 10, 20, pr.Color(0, 0, 0, 255))
-        pr.draw_text(str(f_y), 10, 40, 20, pr.Color(0, 0, 0, 255))
+        pr.draw_text(str(ball.f_x), 10, 10, 20, pr.Color(0, 0, 0, 255))
+        pr.draw_text(str(ball.f_y), 10, 40, 20, pr.Color(0, 0, 0, 255))
 
         pr.end_drawing()
 
